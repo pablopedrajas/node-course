6cef95791c4ea5894c01c25b694d470aa07369b8
pablped <pablped@gmail.com>
2020-11-07 20:07:22 +0100

    some changes task app

--- task-manager/mongodb.js
+++ task-manager/mongodb.js
@@ -4,14 +4,12 @@
 // const MongoClient = mongodb.MongoClient;
 // const ObjectID = mongodb.ObjectID;
 
-const {MongoClient, ObjectID} = require('mongodb') //this is a short way of grabbing the above consts. "destructuring"
+const {MongoClient, ObjectID, ObjectId} = require('mongodb') //this is a short way of grabbing the above consts. "destructuring"
 
 const connectionURL = 'mongodb://127.0.0.1:27017'
 const databaseName = 'task-manager'
 
 const id = new ObjectID();
-console.log(id.id.length)
-console.log(id.toHexString().length)
 
 MongoClient.connect(connectionURL, { useNewUrlParser: true, useUnifiedTopology: true}, (error, client) => {
     if(error) {
@@ -20,55 +18,14 @@ MongoClient.connect(connectionURL, { useNewUrlParser: true, useUnifiedTopology: 
 
     const db = client.db(databaseName)
 
-    // db.collection('users').insertOne({
-    //     _id: id,
-    //     name: 'Vigorous',
-    //     age: 20
-    // }, (error, result) => {
+    // db.collection('tasks').findOne({_id: new ObjectId("5fa6b13f0020b634743a0be8")}, (error, task) => {
     //     if(error) {
-    //         return console.log("Unable to insert user.")
+    //         return console.log(error)
     //     }
-
-    //     console.log(result.ops)
+    //     console.log(task)
     // })
 
-    // db.collection('users').insertMany([
-    //     {
-    //         name:'Jen',
-    //         age: 29
-    //     },
-    //     {
-    //         name: 'Panther',
-    //         age: 27
-    //     }
-    // ], (error, result) => {
-    //     if(error) {
-    //         return console.log('Error inserting documents.')
-    //     }
-
-    //     console.log(result.ops)
-    // })
-
-    // db.collection('tasks').insertMany([
-    //     {
-    //         description: 'Clean the house',
-    //         completed: true
-    //     },
-    //     {
-    //         description: 'Renew inspection',
-    //         completed: false
-    //     },
-    //     {
-    //         description: 'Pot plants',
-    //         completed: false
-    //     }
-    // ], (error, result) => {
-    //     if(error) {
-    //         return console.log('Error inserting documents.')
-    //     }
-
-    //     console.log(result.ops)
-    // })
-
-
+    db.collection('tasks').find({completed: false}).toArray((error, tasks) => {
+        console.log(tasks)
+    })
 })
