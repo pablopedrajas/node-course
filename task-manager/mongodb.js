5e072862b4cc1474509bffbd53b9327c72300c33
pablped <pablped@gmail.com>
2020-11-07 16:45:14 +0100

    task manager updates

--- task-manager/mongodb.js
+++ task-manager/mongodb.js
@@ -1,10 +1,17 @@
 // CRUD create read update delete
 
-const mongodb = require("mongodb");
-const MongoClient = mongodb.MongoClient;
+// const mongodb = require("mongodb");
+// const MongoClient = mongodb.MongoClient;
+// const ObjectID = mongodb.ObjectID;
+
+const {MongoClient, ObjectID} = require('mongodb') //this is a short way of grabbing the above consts. "destructuring"
 
 const connectionURL = 'mongodb://127.0.0.1:27017'
 const databaseName = 'task-manager'
+
+const id = new ObjectID();
+console.log(id.id.length)
+console.log(id.toHexString().length)
 
 MongoClient.connect(connectionURL, { useNewUrlParser: true, useUnifiedTopology: true}, (error, client) => {
     if(error) {
@@ -13,8 +20,55 @@ MongoClient.connect(connectionURL, { useNewUrlParser: true, useUnifiedTopology: 
 
     const db = client.db(databaseName)
 
-    db.collection('users').insertOne({
-        name: 'Licensede',
-        age: 27
-    })
+    // db.collection('users').insertOne({
+    //     _id: id,
+    //     name: 'Vigorous',
+    //     age: 20
+    // }, (error, result) => {
+    //     if(error) {
+    //         return console.log("Unable to insert user.")
+    //     }
+
+    //     console.log(result.ops)
+    // })
+
+    // db.collection('users').insertMany([
+    //     {
+    //         name:'Jen',
+    //         age: 29
+    //     },
+    //     {
+    //         name: 'Panther',
+    //         age: 27
+    //     }
+    // ], (error, result) => {
+    //     if(error) {
+    //         return console.log('Error inserting documents.')
+    //     }
+
+    //     console.log(result.ops)
+    // })
+
+    // db.collection('tasks').insertMany([
+    //     {
+    //         description: 'Clean the house',
+    //         completed: true
+    //     },
+    //     {
+    //         description: 'Renew inspection',
+    //         completed: false
+    //     },
+    //     {
+    //         description: 'Pot plants',
+    //         completed: false
+    //     }
+    // ], (error, result) => {
+    //     if(error) {
+    //         return console.log('Error inserting documents.')
+    //     }
+
+    //     console.log(result.ops)
+    // })
+
+
 })
