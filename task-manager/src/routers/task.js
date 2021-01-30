faad4725fe3baf6b0a3ffe2be567dc5d93feb4d1
licensede <pablped@gmail.com>
2021-01-30 14:54:59 +0100

    pagination and sorting

--- task-manager/src/routers/task.js
+++ task-manager/src/routers/task.js
@@ -18,17 +18,30 @@ router.post('/tasks', auth, async (req, res) => {
 })
 
 // GET /tasks?completed=true
+// GET /tasks?limit=10&skip=10 // pagination
+// GET /tasks?sortBy=createdAt_asc
 router.get('/tasks', auth, async (req, res) => {
     const match = {}
+    const sort = {}
 
-    if(req.query.completed) {
+    if (req.query.completed) {
         match.completed = req.query.completed === 'true'
+    }
+
+    if (req.query.sortBy) {
+        const parts = req.query.sortBy.split(':')
+        sort[parts[0]] = parts[1] === 'desc'
     }
 
     try {
         await req.user.populate({
             path: 'tasks',
-            match
+            match,
+            options: {
+                limit: parseInt(req.query.limit),
+                skip: parseInt(req.query.skip),
+                sort
+            }
         }).execPopulate()
         res.send(req.user.tasks)
     } catch (e) {
